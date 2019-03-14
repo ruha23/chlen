ba84ce06f640a6870ea516775977b3616fefa658
ruha23 <ruha23@ukr.net>
2019-03-14 17:30:37 +0200

    Add files via upload

---
+++ chlen.py
@@ -0,0 +1,6 @@
+#The script determines the length of the penis along the length of the foot, nose, and weight
+a= float(input ("specify foot length in centimeters:"))
+b=float(input("specify the length of the nose:"))
+c=float(input("indicate weight in kilograms:"))
+itog=int(35*(a+(3*b))/c)
+print ("penis length",itog, "centimeters")
